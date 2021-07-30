20cbfdcf2c26656e4027e5823c260c49dba50882
Engineer <52909024+engineeringstudent0012@users.noreply.github.com>
2021-07-30 21:51:00 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import {useRouter} from 'next/router'
 import Image from 'next/image'
 
 import Head from 'next/head'
-// import Footer from '../Components/Footer'
+import Footer from '../Components/Footer'
 import Avatar from "../Components/Avatar"
 import {GlobeIcon} from "@heroicons/react/outline"
 
@@ -88,27 +88,27 @@ export default function Home() {
             </form>
 
             {/*  Footer Component Linked Here */}
-            {/*<Footer/>*/}
-            <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
-                <div className="px-8 py-3">
-                    <p>Nepal</p>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
-                    <div className="flex justify-center items-center md:col-span-2  lg:col-span-1 lg:col-start-2 l">
-                        <GlobeIcon className="h-5 mr-1 text-green-700 "/> Carbon neutral since 2007
-                    </div>
-                    <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
-                        <p>Advertising</p>
-                        <p>Business</p>
-                        <p> How Search Works</p>
-                    </div>
-                    <div className="flex justify-center space-x-8 md:ml-auto">
-                        <p>Privacy</p>
-                        <p> Terms</p>
-                        <p> Settings</p>
-                    </div>
-                </div>
-            </footer>
+            {<Footer/>}
+//             <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+//                 <div className="px-8 py-3">
+//                     <p>Nepal</p>
+//                 </div>
+//                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
+//                     <div className="flex justify-center items-center md:col-span-2  lg:col-span-1 lg:col-start-2 l">
+//                         <GlobeIcon className="h-5 mr-1 text-green-700 "/> Carbon neutral since 2007
+//                     </div>
+//                     <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+//                         <p>Advertising</p>
+//                         <p>Business</p>
+//                         <p> How Search Works</p>
+//                     </div>
+//                     <div className="flex justify-center space-x-8 md:ml-auto">
+//                         <p>Privacy</p>
+//                         <p> Terms</p>
+//                         <p> Settings</p>
+//                     </div>
+//                 </div>
+//             </footer>
         </div>
     )
 }
