4899efab83ac9a437a9308fcf5d5999b044cd183
Ashish Gautam <52909024+engineeringstudent0012@users.noreply.github.com>
2021-12-30 08:34:48 +0545

    Update SearchResults.js

--- Components/SearchResults.js
+++ Components/SearchResults.js
@@ -5,8 +5,7 @@ function SearchResults({results}) {
     return (
         <div className="mx-auto w-full pl-4 pr-3  sm:pl-[5%] md:pl-[14%] lg:pl-52">
             <p
-                className="text-gray-600 text-sm mb-5 mt-3  "
-            >
+                className="text-gray-600 text-sm mb-5 mt-3">
                 About {results.searchInformation?.formattedTotalResults} results
                 ({results.searchInformation?.formattedSearchTime} seconds)</p>
             {results.items?.map(result => (
